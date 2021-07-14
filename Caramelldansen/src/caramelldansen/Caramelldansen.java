210bc137c10dd58cbc6a8431d849e6adb239fe86
Zellity 22 <80797347+Shadowtek1337@users.noreply.github.com>
2021-07-14 16:39:22 -0300

    Add files via upload

---
+++ Caramelldansen/src/caramelldansen/Caramelldansen.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caramelldansen;
+
+/**
+ *
+ * @author ÉozoiomermãoKKKKKKKKK
+ */
+public class Caramelldansen {
+
+    public static void main(String[] args){
+        
+       int c = 3;
+       int e = 4;
+       
+       int[][] numeros=new int[c][e];
+       
+       for (int h=0; h<= c; c++) {
+     for (int j=0; h<= e; e++)
+         System.out.print("Dj aaaaaaaaa😁😁✌️✌️✌️✌️zeitona 🔊🔊🔊🔊puur👍👍👍👍hahahahaha 😃😂😃😂😃pu tch tch tchama pu tch tch tchama pu tch tch tcha🔛🔛 vambora😎😎😎 para✋✋🙅‍♂🙅‍ ♂🙅‍♂🚫🚫 de fala 🗣️🗣️que tu é minha namorrrrrrrrr🥰👩‍❤️‍👨👩‍❤️‍👩👨‍❤️‍👨👩‍❤️‍💋‍👨👩‍❤️‍💋‍👩👨‍❤️‍💋 ‍👨 divulgar no Twitter 🐥🐥🐥🐥que o dj azeitona 🎧🎧🎧amassa💥💥 falando pras novinha👩👩🤭 das casinha 🏠🏠que tu é minha namotututututu🥰👩‍❤️‍👨👩‍❤️‍👨 isso é fofoca 🙅‍♂🙅‍♂🤫 não é prrrrrr🔈🔈🔈to tipo bielzoow comendo🍽️🍽️😏 todas danada🥵🥵😈😈😈 to tipo mc poussss😎😎😎 comendo🍽️🍽️🤭 todas danada 😈😈🥵👉👌não sei🤔🤔🤔 oq aconteceénocomplexx como novinha jogando um tutututu💥💥🎧🎧🔊🔊🔊😁😁👍👍👍👍👍👍👍");
+     }
+       System.out.print("\n");
+       
+     
+    }
+}
